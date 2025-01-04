529f94bbbbbc25043f46f68ba7b0d07c888c708b
L-Benvenuti <luizabenvenuti2017@gmail.com>
2025-01-04 16:41:17 -0600

    Finished Day 6 & 7

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,6 @@
+def Greeting(name):
+    print(f"Welcome {name}!")
+
+
+user_name = input("What's your name? \n")
+Greeting(user_name)
--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,8 +1,50 @@
-word_list = ["aardvark", "baboon", "camel"]
+import random
+import hangman_words, hangman_art
+lives = 6
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+chosen_word = random.choice(hangman_words.word_list)
+print(chosen_word)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+game_over = False
+correct_letters = []
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+
+    print(hangman_art.stages[lives])
+    print(f"****************************{lives}/6 LIVES LEFT****************************")
+
+    if lives == 0:
+        game_over = True
+
+        print(f"\nThe correct word was: {chosen_word}\n***********************YOU LOSE**********************")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+
